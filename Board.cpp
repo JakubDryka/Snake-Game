b16702d8ace6d00085df2281e4cdf7562013c50e
JakubDryka <kuba.dryka@wp.pl>
2020-05-22 17:56:48 +0200

    add actual code with logs

--- Board.cpp
+++ Board.cpp
@@ -40,35 +40,35 @@ void Board::printBoard(Player player1)
 
 
 void Board::makeFrame()
+{
+    for(unsigned int i=0; i < BOARD_HEIGHT ; i++)
     {
-        for(unsigned int i=0; i < BOARD_HEIGHT ; i++)
-        {
-            _board[i][0] = '|';
-            _board[i][BOARD_WIDTH-1] = '|';
-        }
-
-        for(unsigned int j=0; j < BOARD_WIDTH ; j++)
-        {
-            _board[0][j] = '-';
-            _board[BOARD_HEIGHT-1][j] = '-';
-        }
-        _board[0][0] = '+';
-        _board[0][BOARD_WIDTH - 1] = '+';
-        _board[BOARD_HEIGHT - 1][BOARD_WIDTH - 1] = '+';
-        _board[BOARD_HEIGHT - 1][0] = '+';
+        _board[i][0] = '|';
+        _board[i][BOARD_WIDTH-1] = '|';
     }
+
+    for(unsigned int j=0; j < BOARD_WIDTH ; j++)
+    {
+        _board[0][j] = '-';
+        _board[BOARD_HEIGHT-1][j] = '-';
+    }
+    _board[0][0] = '+';
+    _board[0][BOARD_WIDTH - 1] = '+';
+    _board[BOARD_HEIGHT - 1][BOARD_WIDTH - 1] = '+';
+    _board[BOARD_HEIGHT - 1][0] = '+';
+}
 
 void Board::initializeBoard(Player player1)
-    {
-        for(unsigned int i=0; i< BOARD_HEIGHT ; i++)
-            for(unsigned int j=0; j< BOARD_WIDTH; j++)
+{
+    for(unsigned int i=0; i< BOARD_HEIGHT ; i++)
+        for(unsigned int j=0; j< BOARD_WIDTH; j++)
+        {
             {
-                {
-                    _board[i][j] = '.';
-                }
+                _board[i][j] = '.';
             }
-        makeFrame();
-    }
+        }
+    makeFrame();
+}
 
 void Board::generateFood(Player player1)
 {
@@ -84,7 +84,8 @@ void Board::generateFood(Player player1)
                 isInPlayer = true;
             }
         }
-    }while(isInPlayer);
+    }
+    while(isInPlayer);
 
 }
 
@@ -115,4 +116,28 @@ void Board::checkIfPlayerHitHimself(Player &player1)
             gameEnd(player1);
         }
     }
+    std::cout<<"Sprawdzono, nie dosz³o do kolizji";
+}
+
+void Board::printData(Board board, Player player1)
+{
+    std::cout<<"Aktualny kierunek playera to "<<player1._playerDirection<<std::endl;
+    std::cout<<"koordynaty food to "<<board._foodXcord<<" i "<<board._foodYcord<<std::endl;
+    std::cout<<"Glowa weza znajduje sie na "<<player1._snakeBody.front().first<<", "<<player1._snakeBody.front().second<<std::endl;
+    std::cout<<"Rozmiar weza to "<<player1._snakeBody.size()<<std::endl;
+    std::cout<<"Wcisnij ESC aby zamknac"<<std::endl;
+    std::cout<<"Liczba zdobytych punktow: "<<player1._points<<std::endl;
+}
+
+void Board::printStartMenu()
+{
+    std::cout<<std::endl;
+    std::cout<<"Welcome in crazy game called Snake."<<std::endl;
+    std::cout<<std::endl;
+    std::cout<<"If you want to play, just click 'p' button"<<std::endl;
+    std::cout<<"You can play my game using WASD or if you want keyboard arrows"<<std::endl;
+    std::cout<<"Collect food to grow your Snake"<<std::endl;
+    std::cout<<std::endl;
+    std::cout<<"If u want to exit just press ESC button"<<std::endl;
+    std::cout<<"Have fun!"<<std::endl;
 }
--- main.cpp
+++ main.cpp
@@ -8,42 +8,50 @@
 
 int main()
 {
-    srand( time( NULL ) );
-    Player player1;
-    player1.addNewSnakePart();
-
-    Board board;
-    board.initializeBoard(player1);
-    board.printBoard(player1);
-    board.generateFood(player1);
-
     char input1;
-
+    Board board1;
+    board1.printStartMenu();
     do
     {
-        Sleep(500);
         if(_kbhit())
         {
             input1 = getch();
-            player1.useInput(input1);
-            FlushConsoleInputBuffer(GetStdHandle(STD_INPUT_HANDLE));
+            if(input1 == '\x1B')
+            {
+                return 0;
+            }
+            else if(input1 == 'p' || input1 == 'P')
+            {
+                srand( time( NULL ) );
+
+                Player player1;
+                player1.addNewSnakePart();
+
+                board1.initializeBoard(player1);
+                board1.printBoard(player1);
+                board1.generateFood(player1);
+
+                do
+                {
+                    Sleep(500);
+                    if(_kbhit())
+                    {
+                        input1 = getch();
+                        player1.useInput(input1);
+                        FlushConsoleInputBuffer(GetStdHandle(STD_INPUT_HANDLE));
+                    }
+                    player1.movePlayer();
+                    board1.checkIfPlayerHitHimself(player1);
+                    board1.checkIfPlayerAteFood(player1);
+                    board1.printBoard(player1);
+                }
+                while (input1 != '\x1B' && board1.gameEnded == false);
+                board1.gameEnd(player1);
+                return 0;
+            }
         }
-        player1.movePlayer();
-        board.checkIfPlayerAteFood(player1);
-        board.checkIfPlayerHitHimself(player1);
-        board.printBoard(player1);
-
-        std::cout<<"Aktualny kierunek playera to "<<player1._playerDirection<<std::endl;
-        std::cout<<"koordynaty food to "<<board._foodXcord<<" i "<<board._foodYcord<<std::endl;
-        std::cout<<"Glowa weza znajduje sie na "<<player1._snakeBody.front().first<<", "<<player1._snakeBody.front().second<<std::endl;
-        std::cout<<"Rozmiar weza to "<<player1._snakeBody.size()<<std::endl;
-        std::cout<<"Wcisnij ESC aby zamknac"<<std::endl;
-        std::cout<<"Liczba zdobytych punktow: "<<player1._points<<std::endl;
-    } while (input1 != '\x1B' && board.gameEnded == false);
-    board.gameEnd(player1);
-
-
-
+    }
+    while(input1 != '\x1B');
     return 0;
 }
 
